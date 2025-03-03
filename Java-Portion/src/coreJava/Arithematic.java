481a1f6543b15b3d4ba10037ffd79c0f54cfeecd
Tushar Jeena <tushar@whitespectre.com>
2025-03-03 18:59:20 +0530

    expression commit

--- Java-Portion/src/coreJava/Arithematic.java
+++ Java-Portion/src/coreJava/Arithematic.java
@@ -1,54 +1,39 @@
 package coreJava;
 
-import java.util.Scanner;
-
 public class Arithematic {
 	
-	public int add(int i, int j) {
-		int k = i+j;
-		return k;
+	public static int add(int a, int b) { // Method to add 2 integers
+	
+		return a+b;
 	}
 	
-	public int sub(int l, int m) {
-		int n = l-m;
-		return n;
+	public static int sub(int a, int b) { // Method to subtract 2 integers
+		return a-b;
 	}
 	
-	public int mult(int o, int p) {
-		int q = o*p;
-		return q;
+	public static int mult(int a, int b) { // Method to multiply 2 integers
+		return a*b;
 	}
 	
-	public void divd(int r, int s) {
-		int t = r/s;
-		System.out.println("The Final Result is: "+t);
+	public static int divd(int a, int b) { // Method to divide 2 integers
+		return a/b;	
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Enter the value of x1");
-		int x1=scan.nextInt();
 		
-		System.out.println("Enter the value of x2");
-		int x2=scan.nextInt();
+		int sum1 = add(10,2);
 		
-		System.out.println("Enter the value of x3");
-		int x3=scan.nextInt();
+		int sum2 = add(sum1,2);
 		
-		System.out.println("Enter the value of x4");
-		int x4=scan.nextInt();
+		int subt = sub(sum2,2);
 		
-		System.out.println("Enter the value of x5:");
-		int x5=scan.nextInt();
+		int multi = mult(subt,2);
 		
-		Arithematic ob = new Arithematic();
-		int A = ob.add(x1, x2);
-		int B = ob.sub(A, x3);
-		int C = ob.add(B,x4);
-		int D = ob.mult(C, x4);
-		ob.divd(D, x5);		
+		int result = divd(multi,2);
+		
+		System.out.println("The result of the expression is: "+result);
 		
 	}
 
